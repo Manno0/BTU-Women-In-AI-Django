228e7a95e5a27a07f3cceda5f5b76fee12986859
Manno0 <Jeladzemary@gmail.com>
2025-10-28 12:15:50 +0400

    Add blog migrations and update models

--- Backend Development Lecture 9/django_lecture_9/blog/models.py
+++ Backend Development Lecture 9/django_lecture_9/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(verbose_name="სათაური", max_length=255)
     text = models.TextField(verbose_name="ტექსტი" )
+    active =  models.BooleanField(verbose_name="აქტიურია", default=True)
 
     class Meta:
         verbose_name = "blog"
