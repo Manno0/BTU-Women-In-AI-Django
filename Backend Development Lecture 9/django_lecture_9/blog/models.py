c946c2f037d3e2b55ed98aaacf44c2937afbac31
Manno0 <Jeladzemary@gmail.com>
2025-10-28 11:05:20 +0400

    Create Blog Model

--- Backend Development Lecture 9/django_lecture_9/blog/models.py
+++ Backend Development Lecture 9/django_lecture_9/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(verbose_name="სათაური", max_length=255)
+    text = models.TextField(verbose_name="ტექსტი" )
+
+    class Meta:
+        verbose_name = "blog"
+        verbose_name_plural = "blogs"
+
+    def __str__(self):
+        return self.title
