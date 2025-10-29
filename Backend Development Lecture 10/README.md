a212c528056bd95b9dbcc089d31f4c4afbdf58e9
Manno0 <Jeladzemary@gmail.com>
2025-10-29 09:11:50 +0400

    Create Lecture 10 Django project

---
+++ Backend Development Lecture 10/README.md
@@ -0,0 +1,2 @@
+Python Version : Python 3.13.4
+This Project consists of the code done during the Lecture 10 as well as the homework.
--- Backend Development Lecture 9/django_lecture_9/blog/models.py
+++ Backend Development Lecture 9/django_lecture_9/blog/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+
 class Blog(models.Model):
     title = models.CharField(verbose_name="სათაური", max_length=255)
     text = models.TextField(verbose_name="ტექსტი" )
     active =  models.BooleanField(verbose_name="აქტიურია", default=True)
     create_date = models.DateTimeField(verbose_name="შექმნით დრო", auto_now_add=True, null=True)
     update_date = models.DateTimeField(verbose_name="განახლების დრო", auto_now=True, null=True)
-    email = models.EmailField (verbose_name="იმეილი",  unique=True, null=True)
-    website = models.URLField (verbose_name="ვებსაიტი", null=True)
+    email = models.EmailField (verbose_name="იმეილი",  unique=True, null=True, blank=True)
+    website = models.URLField (verbose_name="ვებსაიტი", null=True, blank=True)
+    picture = models.ImageField(upload_to="pictures", null=True, blank=True)
+    files = models.FileField(upload_to="files", null=True, blank=True)
 
     class Meta:
         verbose_name = "blog"
