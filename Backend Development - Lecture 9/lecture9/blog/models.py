fa2180e0f3cf33ea784a7edf82fd0b481700e908
Manno0 <jeladzemary@gmail.com>
2025-12-17 15:26:14 +0400

    Initial migrations

--- Backend Development - Lecture 9/lecture9/blog/models.py
+++ Backend Development - Lecture 9/lecture9/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title= models.CharField(verbose_name= "სათაური",max_length=200)
+    text= models.TextField(verbose_name = "ტექსტი")
+
+
+    class Meta:
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+
+    def __str__(self):
+        return self.title
+
+
+
