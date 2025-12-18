115e14b4edd37770f5002c2fbd416f9d329af9ea
Manno0 <jeladzemary@gmail.com>
2025-12-18 20:47:35 +0400

    Update BlogPost

--- Backend Development - Lecture 9/lecture9/blog/models.py
+++ Backend Development - Lecture 9/lecture9/blog/models.py
@@ -4,10 +4,12 @@ class BlogPost(models.Model):
     title= models.CharField(verbose_name= "სათაური",max_length=200)
     text= models.TextField(verbose_name = "ტექსტი")
     active = models.BooleanField(default= True, verbose_name= "აქტიური")
-    created_at=models.DateTimeField(verbose_name="შექმნის თარიღი",  auto_now_add= True, null= True)
-    updated_at=models.DateTimeField(verbose_name="განახლების თარიღი",auto_now= True, null= True)
+    created_at=models.DateTimeField(verbose_name="შექმნის თარიღი",  auto_now_add= True, null= True, blank= True)
+    updated_at=models.DateTimeField(verbose_name="განახლების თარიღი",auto_now= True, null= True, blank= True)
     email=models.EmailField(verbose_name="იმეილი", unique= True, null= True)
     website=models.URLField(verbose_name="ვებსაიტი", unique= True, null= True)
+    images=models.ImageField(upload_to="images/", null= True, blank= True)
+    documents=models.FileField(upload_to="files/", null= True, blank= True)
 
 
 
