7e3ad2e7c0140e21b8e15eeb956a811c44b15e07
Manno0 <jeladzemary@gmail.com>
2025-12-18 20:06:34 +0400

    Update BlogPost

--- Backend Development - Lecture 9/lecture9/blog/models.py
+++ Backend Development - Lecture 9/lecture9/blog/models.py
@@ -3,14 +3,27 @@ from django.db import models
 class BlogPost(models.Model):
     title= models.CharField(verbose_name= "სათაური",max_length=200)
     text= models.TextField(verbose_name = "ტექსტი")
+    active = models.BooleanField(default= True, verbose_name= "აქტიური")
+    created_at=models.DateTimeField(verbose_name="შექმნის თარიღი",  auto_now_add= True, null= True)
+    updated_at=models.DateTimeField(verbose_name="განახლების თარიღი",auto_now= True, null= True)
+    email=models.EmailField(verbose_name="იმეილი", unique= True, null= True)
+    website=models.URLField(verbose_name="ვებსაიტი", unique= True, null= True)
+
 
 
     class Meta:
         verbose_name = "Blog Post"
         verbose_name_plural = "Blog Posts"
+        ordering = ["title"]
 
     def __str__(self):
         return self.title
+
+class BaseClass(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
 
 
 
