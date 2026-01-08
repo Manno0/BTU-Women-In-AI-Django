0e1f2eb4a596eafd84c74cd0bc7127095f5cb7ea
Manno0 <jeladzemary@gmail.com>
2026-01-09 00:15:49 +0400

    Create and add Author class

--- Backend Development - Lecture 10/lecture10/blog/admin.py
+++ Backend Development - Lecture 10/lecture10/blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Blog
 from .models import BlogImages
+from .models import Author
+
 
 admin.site.register(Blog)
 admin.site.register(BlogImages)
+admin.site.register(Author)
 
 # Register your models here.
--- Backend Development - Lecture 10/lecture10/blog/models.py
+++ Backend Development - Lecture 10/lecture10/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.db import models
 
 class Blog(models.Model):
@@ -19,6 +20,17 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class Author(models.Model):
+    first_name=models.CharField(max_length=200)
+    last_name=models.CharField(max_length=200)
+    blog=models.ForeignKey(Blog,on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+        ordering = ["last_name"]
+
 
 class BlogImages(models.Model):
     image = models.ImageField(upload_to="images/", null= True, blank= True)
