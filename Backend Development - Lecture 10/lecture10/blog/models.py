bae9dd12436c8782049d6df9f09844099582d7e0
Manno0 <jeladzemary@gmail.com>
2026-01-11 23:54:49 +0400

    Add new methods

--- Backend Development - Lecture 10/lecture10/blog/models.py
+++ Backend Development - Lecture 10/lecture10/blog/models.py
@@ -10,7 +10,7 @@ class Blog(models.Model):
     email=models.EmailField(verbose_name="იმეილი", unique= True, null= True)
     website=models.URLField(verbose_name="ვებსაიტი", unique= True, null= True)
     documents=models.FileField(upload_to="files/", null= True, blank= True)
-    authors=models.ManyToManyField(to="Author",verbose_name="ავტორები",related_name="authors")
+    authors=models.ManyToManyField(to="Author",verbose_name="ავტორები",related_name="blogs")
 
 
 
@@ -19,6 +19,9 @@ class Blog(models.Model):
         verbose_name_plural = "Blogs"
         ordering = ["title"]
 
+    def get_images(self):
+        return BlogImages.objects.filter(blog=self)
+
     def __str__(self):
         return self.title
 
@@ -26,12 +29,15 @@ class Author(models.Model):
     first_name=models.CharField(verbose_name="სახელი",  max_length=200)
     last_name=models.CharField(verbose_name="გვარი",max_length=200)
     email=models.EmailField(verbose_name="მეილი", unique= True, null= True)
-    blog=models.ForeignKey(Blog,on_delete=models.CASCADE)
+
+    def get_blogs(self):
+        return self.blogs.all()
 
     class Meta:
         verbose_name = "Author"
         verbose_name_plural = "Authors"
         ordering = ["last_name"]
+
     def __str__(self)  :
         return f'{self.first_name} {self.last_name}'
 
@@ -45,6 +51,8 @@ class BlogImages(models.Model):
         verbose_name_plural = "BlogImages"
         ordering = ["blog"]
 
+
+
     def __str__(self):
         return f'{self.blog.title} - {self.image}'
 
@@ -57,6 +65,8 @@ class BannerImages(models.Model):
         ordering = ["blog"]
     def __str__(self):
         return f'{self.blog.title} - {self.bannerImage}'
+
+
 class BaseClass(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
